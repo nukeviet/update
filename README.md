1eaf178beae29d521bb7026db04dee771f2098af
VINADES.,JSC <contact@vinades.vn>
2018-04-27 09:54:53 +0700

    Cập nhật gói nâng cấp

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Hướng dẫn cập nhật từ 4.3.00 lên NukeViet 4.3.01
+# Hướng dẫn cập nhật từ 4.3.00, 4.3.01 lên NukeViet 4.3.02
 
 Nếu phiên bản NukeViet 4 của bạn nhỏ hơn 4.3.00 bạn cần tìm hướng dẫn nâng cấp lên phiên bản 4.3.00 trước khi tiến hành các bước tiếp theo.
 
@@ -24,5 +24,45 @@ Nếu thất bại hãy thử cách cập nhật thủ công bên dưới.
 
 #### Cập nhật thủ công:
 
-Download gói cập nhật tại: https://github.com/nukeviet/update/releases/download/to-4.3.01/update-to-4.3.01.zip
+Download gói cập nhật tại: https://github.com/nukeviet/update/releases/download/to-4.3.02/update-to-4.3.02.zip
 Giải nén và Upload các file trong gói cập nhật với cấu trúc của NukeViet, sau đó vào admin để tiến hành cập nhật.
+
+### Bước 3: Điều chỉnh giao diện
+
+Nếu website của bạn sử dụng giao diện không phải mặc định thì thực hiện cập nhật theo hướng dẫn sau
+
+> Các hướng dẫn dưới đây bạn có thể không cần thực hiện nếu cảm thấy khó khăn. Việc không cập nhật giao diện website của bạn vẫn hoạt động bình thường.
+
+**Tìm trong file: themes/my_theme/js/main.js:**
+
+```html
+window.location.href = location.reload()
+```
+
+Nếu có thay bằng
+
+```html
+location.reload();
+```
+
+**Nếu giao diện của bạn tồn tại themes/my_theme/modules/contact/form.tpl**
+
+Mở form.tpl tìm
+
+```html
+        <div class="form-group">
+            <label><input type="checkbox" name="sendcopy" value="1" checked="checked" /><span>{LANG.sendcopy}</span></label>
+        </div>
+```
+
+Thêm lên trên
+
+```html
+        <!-- BEGIN: sendcopy -->
+```
+
+Thêm xuống dưới
+
+```html
+        <!-- END: sendcopy -->
+```
--- install/update_data.php
+++ install/update_data.php
@@ -17,17 +17,17 @@ $nv_update_config = array();
 $nv_update_config['type'] = 1;
 
 // ID goi cap nhat
-$nv_update_config['packageID'] = 'NVUD4301';
+$nv_update_config['packageID'] = 'NVUD4302';
 
 // Cap nhat cho module nao, de trong neu la cap nhat NukeViet, ten thu muc module neu la cap nhat module
 $nv_update_config['formodule'] = '';
 
 // Thong tin phien ban, tac gia, ho tro
-$nv_update_config['release_date'] = 1517475600;
+$nv_update_config['release_date'] = 1525251600;
 $nv_update_config['author'] = 'VINADES.,JSC <contact@vinades.vn>';
-$nv_update_config['support_website'] = 'https://github.com/nukeviet/update/tree/to-4.3.01';
-$nv_update_config['to_version'] = '4.3.01';
-$nv_update_config['allow_old_version'] = array('4.3.00');
+$nv_update_config['support_website'] = 'https://github.com/nukeviet/update/tree/to-4.3.02';
+$nv_update_config['to_version'] = '4.3.02';
+$nv_update_config['allow_old_version'] = array('4.3.00', '4.3.01');
 
 // 0:Nang cap bang tay, 1:Nang cap tu dong, 2:Nang cap nua tu dong
 $nv_update_config['update_auto_type'] = 1;
@@ -39,12 +39,18 @@ $nv_update_config['lang']['en'] = array();
 // Tiếng Việt
 $nv_update_config['lang']['vi']['nv_up_modnews4301'] = 'Cập nhật module news lên 4.3.01';
 $nv_update_config['lang']['vi']['nv_up_modlang4301'] = 'Cập nhật module language lên 4.3.01';
-$nv_update_config['lang']['vi']['nv_up_finish'] = 'Cập nhật CSDL lên phiên bản 4.3.01';
+$nv_update_config['lang']['vi']['nv_up_modusers4302'] = 'Cập nhật module users lên 4.3.02';
+$nv_update_config['lang']['vi']['nv_up_modcontact4302'] = 'Cập nhật module contact lên 4.3.02';
+$nv_update_config['lang']['vi']['nv_up_sys4302'] = 'Cập nhật hệ thống lên 4.3.02';
+$nv_update_config['lang']['vi']['nv_up_finish'] = 'Cập nhật CSDL lên phiên bản 4.3.02';
 
 // English
 $nv_update_config['lang']['en']['nv_up_modnews4301'] = 'Update module news to 4.3.01';
 $nv_update_config['lang']['en']['nv_up_modlang4301'] = 'Update module language to 4.3.01';
-$nv_update_config['lang']['en']['nv_up_finish'] = 'Update new version 4.3.01';
+$nv_update_config['lang']['en']['nv_up_modusers4302'] = 'Update module users to 4.3.02';
+$nv_update_config['lang']['en']['nv_up_modcontact4302'] = 'Update module contact to 4.3.02';
+$nv_update_config['lang']['en']['nv_up_sys4302'] = 'Update system to 4.3.02';
+$nv_update_config['lang']['en']['nv_up_finish'] = 'Update new version 4.3.02';
 
 $nv_update_config['tasklist'] = array();
 $nv_update_config['tasklist'][] = array(
@@ -60,7 +66,25 @@ $nv_update_config['tasklist'][] = array(
     'f' => 'nv_up_modlang4301'
 );
 $nv_update_config['tasklist'][] = array(
-    'r' => '4.3.01',
+    'r' => '4.3.02',
+    'rq' => 2,
+    'l' => 'nv_up_modusers4302',
+    'f' => 'nv_up_modusers4302'
+);
+$nv_update_config['tasklist'][] = array(
+    'r' => '4.3.02',
+    'rq' => 2,
+    'l' => 'nv_up_modcontact4302',
+    'f' => 'nv_up_modcontact4302'
+);
+$nv_update_config['tasklist'][] = array(
+    'r' => '4.3.02',
+    'rq' => 2,
+    'l' => 'nv_up_sys4302',
+    'f' => 'nv_up_sys4302'
+);
+$nv_update_config['tasklist'][] = array(
+    'r' => '4.3.02',
     'rq' => 2,
     'l' => 'nv_up_finish',
     'f' => 'nv_up_finish'
@@ -147,6 +171,135 @@ function nv_up_modlang4301()
         }
     } catch (PDOException $e) {
         trigger_error($e->getMessage());
+    }
+
+    return $return;
+}
+
+/**
+ * nv_up_modusers4302()
+ *
+ * @return
+ *
+ */
+function nv_up_modusers4302()
+{
+    global $nv_update_baseurl, $db, $db_config, $nv_Cache, $global_config, $nv_update_config;
+    $return = array(
+        'status' => 1,
+        'complete' => 1,
+        'next' => 1,
+        'link' => 'NO',
+        'lang' => 'NO',
+        'message' => ''
+    );
+    // Duyệt tất cả các ngôn ngữ
+    foreach ($global_config['allow_sitelangs'] as $lang) {
+        // Lấy tất cả các module và module ảo của nó
+        $mquery = $db->query("SELECT title, module_data FROM " . $db_config['prefix'] . "_" . $lang . "_modules WHERE module_file = 'users'");
+        while (list ($mod, $mod_data) = $mquery->fetch(3)) {
+            // Thêm trường email_verification_time
+            try {
+                $db->query("ALTER TABLE " . $db_config['prefix'] . "_" . $mod_data . " ADD email_verification_time INT(11) NOT NULL DEFAULT '-1' COMMENT '-3: Tài khoản sys, -2: Admin kích hoạt, -1 không cần kích hoạt, 0: Chưa xác minh, > 0 thời gian xác minh' AFTER safekey;");
+            } catch (PDOException $e) {
+                trigger_error($e->getMessage());
+            }
+        }
+    }
+    return $return;
+}
+
+/**
+ * nv_up_modcontact4302()
+ *
+ * @return
+ *
+ */
+function nv_up_modcontact4302()
+{
+    global $nv_update_baseurl, $db, $db_config, $nv_Cache, $global_config, $nv_update_config;
+    $return = array(
+        'status' => 1,
+        'complete' => 1,
+        'next' => 1,
+        'link' => 'NO',
+        'lang' => 'NO',
+        'message' => ''
+    );
+    // Duyệt tất cả các ngôn ngữ
+    foreach ($global_config['allow_sitelangs'] as $lang) {
+        // Lấy tất cả các module và module ảo của nó
+        $mquery = $db->query("SELECT title, module_data FROM " . $db_config['prefix'] . "_" . $lang . "_modules WHERE module_file = 'contact'");
+        while (list ($mod, $mod_data) = $mquery->fetch(3)) {
+            // Thêm trường từ khóa
+            try {
+                $db->query("INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', '" . $mod . "', 'sendcopymode', '0');");
+            } catch (PDOException $e) {
+                trigger_error($e->getMessage());
+            }
+            try {
+                $db->query("INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', '" . $mod . "', 'bodytext', '');");
+            } catch (PDOException $e) {
+                trigger_error($e->getMessage());
+            }
+        }
+    }
+    return $return;
+}
+
+/**
+ * nv_up_sys4302()
+ *
+ * @return
+ *
+ */
+function nv_up_sys4302()
+{
+    global $nv_update_baseurl, $db, $db_config, $nv_Cache, $global_config, $nv_update_config;
+    $return = array(
+        'status' => 1,
+        'complete' => 1,
+        'next' => 1,
+        'link' => 'NO',
+        'lang' => 'NO',
+        'message' => ''
+    );
+
+    // Cấu hình giao diện cho từng quản trị
+    try {
+        $db->query("ALTER TABLE " . NV_AUTHORS_GLOBALTABLE . " ADD admin_theme VARCHAR(100) NOT NULL DEFAULT '' AFTER main_module;");
+    } catch (PDOException $e) {
+        trigger_error($e->getMessage());
+    }
+    // Chức năng debug
+    try {
+        $db->query("INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('sys', 'define', 'nv_debug', '0');");
+    } catch (PDOException $e) {
+        trigger_error($e->getMessage());
+    }
+    // Chức năng cấu hình IP không kiểm tra flood
+    try {
+        $db->query("RENAME TABLE " . $db_config['prefix'] . "_banip TO " . $db_config['prefix'] . "_ips;");
+    } catch (PDOException $e) {
+        trigger_error($e->getMessage());
+    }
+    try {
+        $db->query("ALTER TABLE " . $db_config['prefix'] . "_ips ADD type tinyint(4) UNSIGNED NOT NULL DEFAULT '0' AFTER id;");
+    } catch (PDOException $e) {
+        trigger_error($e->getMessage());
+    }
+    try {
+        $db->query("ALTER TABLE " . $db_config['prefix'] . "_ips DROP INDEX ip, ADD UNIQUE ip (ip, type) USING BTREE;");
+    } catch (PDOException $e) {
+        trigger_error($e->getMessage());
+    }
+    // Xem trước giao diện
+    foreach ($global_config['allow_sitelangs'] as $lang) {
+        try {
+            $db->query("INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', 'global', 'preview_theme', '');");
+        } catch (PDOException $e) {
+            trigger_error($e->getMessage());
+        }
     }
 
     return $return;
