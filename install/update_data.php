ddbdd373ac2a0d0647219a796044032096f7a661
VINADES.,JSC <contact@vinades.vn>
2021-07-16 21:15:59 +0700

    Update For NukeViet 4.4.03

--- install/update_data.php
+++ install/update_data.php
@@ -337,6 +337,52 @@ function nv_up_sys4403()
         }
     }
 
+    //API
+    try {
+        $db->query("CREATE TABLE " . $db_config['prefix'] . "_authors_api_role (
+                role_id smallint(4) NOT NULL AUTO_INCREMENT,
+                role_title varchar(250) NOT NULL DEFAULT '',
+                role_description text NOT NULL,
+                role_data text NOT NULL,
+                addtime int(11) NOT NULL DEFAULT '0',
+                edittime int(11) NOT NULL DEFAULT '0',
+                PRIMARY KEY (role_id)
+            ) ENGINE = MYISAM COMMENT 'Bảng lưu quyền truy cập API'");
+    } catch (PDOException $e) {
+        trigger_error(print_r($e, true));
+    }
+
+    try {
+        $db->query("CREATE TABLE " . $db_config['prefix'] . "_authors_api_credential (
+                admin_id int(11) UNSIGNED NOT NULL,
+                credential_title varchar(255) NOT NULL DEFAULT '',
+                credential_ident varchar(50) NOT NULL DEFAULT '',
+                credential_secret varchar(250) NOT NULL DEFAULT '',
+                credential_ips varchar(255) NOT NULL DEFAULT '',
+                api_roles varchar(255) NOT NULL DEFAULT '',
+                addtime int(11) NOT NULL DEFAULT '0',
+                edittime int(11) NOT NULL DEFAULT '0',
+                last_access int(11) NOT NULL DEFAULT '0',
+                UNIQUE KEY credential_ident (credential_ident),
+                UNIQUE KEY credential_secret (credential_secret),
+                KEY admin_id (admin_id)
+            ) ENGINE = MYISAM COMMENT 'Bảng lưu key API của quản trị'");
+    } catch (PDOException $e) {
+        trigger_error(print_r($e, true));
+    }
+
+    try {
+        $db->query("INSERT INTO " . $db_config['prefix'] . "_config (lang, module, config_name, config_value) VALUES ('sys', 'global', 'remote_api_access', '0')");
+    } catch (PDOException $e) {
+        trigger_error(print_r($e, true));
+    }
+
+    try {
+        $db->query("INSERT INTO " . $db_config['prefix'] . "_config (lang, module, config_name, config_value) VALUES ('sys', 'global', 'remote_api_log', '1')");
+    } catch (PDOException $e) {
+        trigger_error(print_r($e, true));
+    }
+
     //Tao lai config_global.php
     nv_save_file_config_global();
 
