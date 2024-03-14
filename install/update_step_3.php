0ba4213dd3c3936bce61a8621232b6e53e568d0d
VINADES.,JSC <contact@vinades.vn>
2024-03-14 10:26:19 +0700

    Cập nhật gói nâng cấp

--- install/update_step_3.php
+++ install/update_step_3.php
@@ -159,6 +159,10 @@ nv_deletefile(NV_ROOTDIR . '/vendor/endroid', true);
 nv_deletefile(NV_ROOTDIR . '/vendor/pclzip', true);
 nv_deletefile(NV_ROOTDIR . '/vendor/symfony/options-resolver', true);
 
+// File thừa bản 4.4.07 => 4.4.08
+nv_deletefile(NV_ROOTDIR . '/assets/editors/ckeditor/bender-runner.config.json');
+nv_deletefile(NV_ROOTDIR . '/assets/editors/ckeditor/plugins/googledocs', true);
+
 // Tự xóa chính nó
 nv_deletefile(NV_ROOTDIR . '/install/update_step_3.php');
 
